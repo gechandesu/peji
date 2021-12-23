9ff57918dd00a86bae3d622d48e61beacebe8468
gd <gechandev@gmail.com>
2021-12-23 21:22:42 +0300

    update setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as long_descr:
 setup(
     name='Peji',
     version='1.0',
-    author='gd',
+    author='ge',
     description='Static site generator.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,11 +20,11 @@ setup(
     python_requires='>=3.6',
     py_modules=['peji'],
     install_requires = [
-        'click>=7.1.2',
-        'markdown>=3.3.4',
-        'Jinja2>=2.11.3',
-        'Pygments>=2.8.1',
-        'PyYAML>=5.4.1'
+        'click',
+        'markdown',
+        'Jinja2',
+        'Pygments',
+        'PyYAML'
     ],
     entry_points = {
         'console_scripts': [
