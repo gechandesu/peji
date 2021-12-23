0e277fcd663ef152f67bc814d39de7bd7c98ab3f
gd <gechandev@gmail.com>
2021-12-23 21:38:03 +0300

    fix syntax and update version

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as long_descr:
     long_description = long_descr.read()
 
 setup(
-    name='Peji',
-    version='1.0.1',
-    author='ge',
-    description='Static site generator.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://peji.gch.icu/',
-    classifiers=[
+    name = 'Peji',
+    version = '1.0.2',
+    author = 'ge',
+    description = 'Static site generator.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://peji.gch.icu/',
+    classifiers = [
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    py_modules=['peji'],
+    python_requires = '>=3.6',
+    py_modules = ['peji'],
     install_requires = [
         'click',
         'markdown',
